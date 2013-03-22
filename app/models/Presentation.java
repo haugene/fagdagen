6e034edf10fdee084f36448b6c0d66fdc626eae1
Kristian Haugene <kristian.haugene@capgemini.com>
2013-03-22 22:14:37 +0100

    added a method to retrieve presentations by slot

--- app/models/Presentation.java
+++ app/models/Presentation.java
@@ -6,6 +6,7 @@ import play.db.ebean.Model;
 import javax.persistence.Entity;
 import javax.persistence.Id;
 import javax.persistence.ManyToOne;
+import java.util.*;
 
 /**
  * Represents a presentation.
@@ -40,10 +41,58 @@ public class Presentation extends Model {
     @ManyToOne
     public Track track;
 
-    // Rank within slot
+    // Rank within slot and track
     @Constraints.Required
     public Long rank;
 
     // Is this a presentation for all?(keynotes etc)
     public boolean spanAllTracks = false;
+
+    // Create a Finder for Presentation objects
+    public static Finder<Long, Presentation> find = new Finder(Long.class, Presentation.class);
+
+    /**
+     * A method for reading presentations in an orderly fashion
+     * @return a map of all the slots and the presentations that is attributed to it
+     */
+    public Map<Slot, List<Presentation>> getPresentationsBySlot()
+    {
+        // Get all the presentations
+        List<Presentation> presentations = find.all();
+
+        // Find all distinct slots
+        Set<Slot> slots = new HashSet<Slot>();
+        for(Presentation presentation : presentations)
+        {
+            slots.add(presentation.slot);
+        }
+
+        /*
+         * Now, create a map of all slots and their presentations.
+         * We want the map to be sorted, let's go for a TreeMap
+         * // TODO: kristian : 22.03.2013 : There is no sorting atm. Make Slot comparable and so on.
+         */
+        Map<Slot, List<Presentation>> result = new TreeMap<Slot, List<Presentation>>();
+
+        // Then add, add and add stuff
+        for(Slot slot : slots)
+        {
+            List<Presentation> presentationsForSlot = new ArrayList<Presentation>();
+
+            // Find all presentations for this slot
+            for(Presentation presentation : presentations)
+            {
+                if(presentation.slot.equals(slot))
+                {
+                    presentationsForSlot.add(presentation);
+                }
+            }
+
+            // Then add the list to the map, with this Slot as key
+            result.put(slot, presentationsForSlot);
+        }
+
+        // Our work here is done, return it
+        return result;
+    }
 }
---
+++ app/utils/InitialDataUtil.java
@@ -0,0 +1,11 @@
+package utils;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: developer
+ * Date: 3/22/13
+ * Time: 9:37 PM
+ * To change this template use File | Settings | File Templates.
+ */
+public class InitialDataUtil {
+}
