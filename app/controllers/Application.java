10aa4f5d3d944a3dbb59b96bc5db31efc3846a3f
Kristian Haugene <kristian.haugene@capgemini.com>
2013-03-22 22:28:17 +0100

    added functionality for adding sample data through a special url addData

--- app/controllers/Application.java
+++ app/controllers/Application.java
@@ -4,20 +4,38 @@ import models.Track;
 import org.apache.commons.lang3.StringUtils;
 import play.mvc.*;
 
+import utils.InitialDataUtil;
 import views.html.*;
 
 public class Application extends Controller {
   
-    public static Result index() {
-        if(StringUtils.isNotBlank(session("username")))
-        {
-            System.out.println("Logged in as: " + session("username"));
-        } else
-        {
-            System.out.println("User is not logged in");
-        }
-
+    public static Result index()
+    {
         return ok(index.render(Track.findAll()));
     }
-  
+
+    public static Result addData()
+    {
+        if(isUserLoggedIn())
+        {
+            InitialDataUtil.addInitialData();
+        }
+
+        // The user has to log in
+        flash("success", "You need to log in before adding data. Log in and try again");
+        return Admin.login();
+    }
+
+    /**
+     * Checks the session to see if the user is logged in
+     * @return true if user is logged in, false otherwise
+     */
+    public static Boolean isUserLoggedIn()
+    {
+        if(StringUtils.isNotBlank(session("username")))
+        {
+            return true;
+        }
+        return  false;
+    }
 }
--- app/utils/InitialDataUtil.java
+++ app/utils/InitialDataUtil.java
@@ -1,11 +1,45 @@
 package utils;
 
+import com.avaje.ebean.Ebean;
+import models.Slot;
+import org.joda.time.DateTime;
+import play.Logger;
+
 /**
- * Created with IntelliJ IDEA.
- * User: developer
- * Date: 3/22/13
- * Time: 9:37 PM
- * To change this template use File | Settings | File Templates.
+ * This utility class will add some slots and presentations to the database
  */
 public class InitialDataUtil {
+
+    public static void addInitialData()
+    {
+        Logger.of(InitialDataUtil.class).info("Adding initial data");
+
+        addSlots();
+    }
+
+    private static void addSlots()
+    {
+        addSlot(9, 0, null);
+    }
+
+    /**
+     * Adds a slot to the database.
+     * @param hour the hour of the day for the presentation
+     * @param minute the minute of the hour for the presentation
+     * @param duration duration of the slot in minutes, can be null. Defaults to 30 min
+     */
+    private static void addSlot(Integer hour, Integer minute, Integer duration)
+    {
+        Slot slot = new Slot();
+        slot.startTime = new DateTime(2013, 4, 25, hour, minute).toDate();
+        if(duration != null)
+        {
+            slot.endTime = new DateTime(slot.startTime).plusMinutes(duration).toDate();
+        } else
+        {
+            slot.endTime = new DateTime(slot.startTime).plusMinutes(30).toDate();
+        }
+
+        Ebean.save(slot);
+    }
 }
