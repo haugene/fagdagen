a022fad0ffbd7ae10f32a439f5adc5ec5deefed8
Daniel <daniel.n.carlsen@gmail.com>
2013-03-30 23:59:45 +0100

    Don't allow blank username and password. Doesn't work well if it's used to create a new user

--- app/controllers/Admin.java
+++ app/controllers/Admin.java
@@ -22,6 +22,11 @@ public class Admin extends Controller {
         public String password;
 
         public String validate() {
+            // Don't allow blank login
+            if(username.isEmpty() || password.isEmpty()) {
+                return "Username or password cannot be blank";
+            }
+
             if(User.authenticate(username, password) == null) {
                 return "Invalid user or password";
             }
