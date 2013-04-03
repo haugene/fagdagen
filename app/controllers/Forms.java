7de9b702a3068b387399d617e4fe3e672869dc22
Kristian Haugene <kristian.haugene@capgemini.com>
2013-04-03 20:39:50 +0200

    added a fallback error message for empty forms. we should use client side validation

--- app/controllers/Forms.java
+++ app/controllers/Forms.java
@@ -1,23 +1,27 @@
 package controllers;
 
 import com.avaje.ebean.Ebean;
-import domain.forms.AddPresentation;
 import models.Presentation;
 import models.Slot;
 import models.Track;
+import org.apache.commons.lang3.StringUtils;
 import play.data.DynamicForm;
-import play.data.Form;
 import play.mvc.Controller;
 import play.mvc.Result;
-import views.html.modals.newPresentationModal;
+import static org.apache.commons.lang3.StringUtils.isBlank;
 
+import static org.apache.commons.lang3.StringUtils.isNotBlank;
 import static play.data.Form.form;
 
 /**
- * Controller for operations on presentations
+ * Controller for form post actions
  */
 public class Forms extends Controller {
 
+    /**
+     * Reads a HTML form dynamically. Retrieves fields for a presentation, and stores it in the database.
+     * @return redirect to index
+     */
     public static Result savePresentation() {
         DynamicForm requestData = form().bindFromRequest();
 
@@ -25,28 +29,57 @@ public class Forms extends Controller {
         String presenter = requestData.get("presenter");
         String description = requestData.get("description");
 
-        Long slotId = Long.parseLong(requestData.get("slot"));
-        Long trackId = Long.parseLong(requestData.get("track"));
+        Long slotId = null;
+        Long trackId = null;
+        Integer rank = null;
+        // Added try/catch to parsing.
+        try {
+            slotId = Long.parseLong(requestData.get("slot"));
+            trackId = Long.parseLong(requestData.get("track"));
+            rank = Integer.parseInt(requestData.get("rank"));
+        } catch (Exception e)
+        {
+            // Do nothing, let the values be null
+        }
 
-        Slot slot = Slot.find.byId(slotId);
-        Track track = Track.find.byId(trackId);
+        if(isBlank(name) || isBlank(presenter) || isBlank(description) || isNull(slotId) || isNull(trackId) || isNull(rank))
+        {
+            flash("form_result", "Could not save presentation, there were empty fields");
+        } else
+        {
+            Slot slot = Slot.find.byId(slotId);
+            Track track = Track.find.byId(trackId);
 
-        int rank = Integer.parseInt(requestData.get("rank"));
-
-        Presentation presentation = new Presentation(name, description, presenter, slot, track, rank);
-        Ebean.save(presentation);
+            Presentation presentation = new Presentation(name, description, presenter, slot, track, rank);
+            Ebean.save(presentation);
+        }
 
         return redirect(routes.Application.index());
     }
 
+    /**
+     * Reads a HTML form dynamically. Retrieves the id of a presentation and deletes it from the database.
+     * @return redirect to index
+     */
     public static Result deletePresentation() {
+
         DynamicForm requestData = form().bindFromRequest();
-        Long id = Long.parseLong(requestData.get("presentationId"));
 
-        Presentation presentation = Presentation.find.byId(id);
-
-        Ebean.delete(presentation);
+        // Might throw exception parsing number
+        try {
+            Long id = Long.parseLong(requestData.get("presentationId"));
+            Presentation presentation = Presentation.find.byId(id);
+            Ebean.delete(presentation);
+        }catch (Exception e)
+        {
+            flash("form_result", "Could not delete presentation, could not parse ID");
+        }
 
         return redirect(routes.Application.index());
+    }
+
+    private static boolean isNull(Object object)
+    {
+        return object == null;
     }
 }
