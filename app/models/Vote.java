6c4c57ee8c286303057824332a36e19291498f99
Kristian Haugene <kristian.haugene@capgemini.com>
2013-04-13 21:44:19 +0200

    unused import

---
+++ app/models/Vote.java
@@ -0,0 +1,11 @@
+package models;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: developer
+ * Date: 4/13/13
+ * Time: 9:39 PM
+ * To change this template use File | Settings | File Templates.
+ */
+public class Vote {
+}
